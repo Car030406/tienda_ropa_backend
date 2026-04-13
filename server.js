815558c8882dcc7e2490d6d56f34664a070ec12e
Caro Carrete <abril.carrete.itid@unipolidgo.edu.mx>
2026-04-12 19:09:30 -0600

    fix: puerto para render

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const db = require('./db');
 const cors = require('cors');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middlewares
 app.use(cors());
